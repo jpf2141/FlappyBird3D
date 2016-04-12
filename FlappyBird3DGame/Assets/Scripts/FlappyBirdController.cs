82cb0f7052bb86a578fa350d4d4bd840551bdec8
kdj2109 <kdj2109@columbia.edu>
2016-04-11 23:17:33 -0400

    created flappy bird game object and controller script

---
+++ FlappyBird3DGame/Assets/Scripts/FlappyBirdController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlappyBirdController : MonoBehaviour {
+	public float up_force;
+	public GameObject flappybird;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fly ();
+	}
+
+	private void fly() { 
+		if (Input.GetMouseButtonDown (0)) {
+			flyup ();
+		}
+	}
+
+	private void flyup() { 
+		Rigidbody rigidbody = getRigidbody ();
+		rigidbody.velocity = Vector3.zero;  // Prevent flappy bird from zooming upwards
+		rigidbody.AddForce (new Vector3(0.0F, up_force, 0.0F));
+	}
+
+	private Rigidbody getRigidbody() { 
+		return flappybird.GetComponent<Rigidbody> ();
+	}
+}
