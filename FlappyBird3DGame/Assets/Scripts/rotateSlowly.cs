51bc03f7be524108e24787240278d9088c7e1110
Christopher Chang <christopherchang@dyn-209-2-231-22.dyn.columbia.edu>
2016-04-27 11:12:46 -0400

    treasure chest has texture, rotates at slower speed

---
+++ FlappyBird3DGame/Assets/Scripts/rotateSlowly.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public Vector3 tempPosition;
+
+	// Use this for initialization
+	void Start () {
+		tempPosition = transform.localPosition;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0.0f, 125*Time.deltaTime, 0.0f); 
+	}
+}
