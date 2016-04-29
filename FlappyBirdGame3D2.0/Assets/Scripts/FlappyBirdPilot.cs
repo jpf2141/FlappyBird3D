73b2c3078cc50c2dec635553a3e8a1bc30456bc3
kdj2109 <kdj2109@columbia.edu>
2016-04-29 03:28:58 -0400

    initial changes to the ui -- flappy bird game 3D 2.0

---
+++ FlappyBirdGame3D2.0/Assets/Scripts/FlappyBirdPilot.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlappyBirdPilot : MonoBehaviour {
+	public GameObject joystick;
+	public float y_force; 
+	public float z_force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			gameObject.transform.position += Vector3.up * .1F;
+		} else if (Input.GetKey (KeyCode.DownArrow)) { 
+			gameObject.transform.position += Vector3.down * .1F;
+		} else if (Input.GetKey (KeyCode.RightArrow)) { 
+			gameObject.transform.position += Vector3.back * .1F;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) { 
+			gameObject.transform.position += Vector3.forward * .1F;
+		}
+
+		gameObject.transform.rotation = joystick.transform.rotation;
+		fly (joystick.transform.root.eulerAngles);
+	}
+
+	private void fly (Vector3 direction) { 
+		direction = new Vector3 (0, getDirection(direction.y), getDirection(direction.z));
+
+		gameObject.transform.position += direction * .01F;
+
+		Debug.Log (direction);
+	}
+
+	private float getDirection(float angle) {
+		Debug.Log (angle);
+		if (angle % 360 > 0) { 
+			return -1.0F;
+		} else if (angle % 360 == 0) { 
+			return 0.0F; 
+		}
+
+		return 1.0F; 
+	}
+
+
+	private Rigidbody getRigidbody() { 
+		return gameObject.GetComponent<Rigidbody> ();
+	}
+}
---
+++ FlappyBirdGame3D2.0/Assets/Scripts/PipeController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipeController : MonoBehaviour {
+	public float start; 
+	public float middle; 
+	public float end; 
+
+	public GameObject track;
+
+	private bool hasDuplicated = false; 
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.x < middle * (1.0F / transform.localScale.x) && !hasDuplicated) {
+			createNewPipes ();
+			hasDuplicated = true;
+		} else if (gameObject.transform.position.x < end * (1.0F / transform.localScale.x)) { 
+			Destroy (gameObject);
+		}
+
+		gameObject.transform.position += gameObject.transform.right * -1.5f * Time.deltaTime;
+	}
+
+	private void createNewPipes() { 
+		GameObject clone = (GameObject) Instantiate (gameObject, 
+			new Vector3(start * (1.0F / transform.localScale.x), 
+				Random.Range(-2F, 1F) * (1.0F/ transform.localScale.y), 
+				gameObject.transform.position.z), 
+			transform.rotation);
+		clone.transform.parent = track.transform; 
+		clone.transform.localScale = gameObject.transform.localScale;
+	}
+}
+
