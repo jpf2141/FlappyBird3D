cf48dd0f4d9dd742196c4fdfeecfff43207d3762
kdj2109 <kdj2109@columbia.edu>
2016-04-30 17:56:47 -0400

    added first person minimap

---
+++ FlappyBirdGame3D2.0/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+	public GameObject flappybird;
+
+
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.position = flappybird.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
