26e6ddfbb5cf3171adb9fea268f8f76f8f861249
kdj2109 <kdj2109@columbia.edu>
2016-05-03 01:16:17 -0400

    menu scene

---
+++ FlappyBirdGame3D2.0/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 || Input.GetMouseButtonDown (0)) { 
+			Application.LoadLevel("Game");
+		}
+	}
+}
