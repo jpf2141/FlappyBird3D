603d054d3861fb9117ea807af6374572c6f2634c
kdj2109 <kdj2109@columbia.edu>
2016-05-05 05:10:19 -0400

    watching lord of the rings while coding at 5 in the morning

--- FlappyBirdGame3D2.0/Assets/Scripts/FirstPersonController.cs
+++ FlappyBirdGame3D2.0/Assets/Scripts/FirstPersonController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class FirstPersonController : MonoBehaviour {
 	public GameObject flappybird;
-
+	public float delay = 1.0F;
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +12,40 @@ public class FirstPersonController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		scanForTargets ();
+	}
+
+	private void scanForTargets () { 
+		Ray ray = new Ray (gameObject.transform.position, gameObject.transform.forward);
+
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, 200)) {
+			if (hit.collider.gameObject != null) { 
+				if (checkTarget(hit.collider.gameObject)) {
+					selectTarget(hit.collider.gameObject); 
+					Destroy(hit.collider.gameObject, 0.5f);
+				}
+			}
+		}
+	}
+
+	private void selectTarget(GameObject target) { 
+		Material selectedMaterial = Resources.Load("SelectionMaterial", typeof(Material)) as Material;
+		Renderer[] renderers = target.gameObject.GetComponentsInChildren<Renderer> ();
+
+		for (int i = 0; i < renderers.Length; i++) { 
+			renderers [i].material = selectedMaterial;
+		}
+
+		target.gameObject.GetComponentsInChildren<Renderer> ();
+	}
+
+	private bool checkTarget(GameObject target) { 
+		if (target.gameObject.CompareTag ("Cloud")) {
+			return true;
+		}
+
+		return false;
 	}
 }
